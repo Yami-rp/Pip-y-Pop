dcd10a088683bf95094abd51380c8ad82d797fdc
Elena López <elena.lopez2024@hilasal.superate.org.sv>
2025-03-06 14:12:20 -0600

    Karaoke

---
+++ main.js
@@ -0,0 +1,36 @@
+// Conexión con HTMl (Seleccionar elementos)
+
+const songForm = document.getElementById('song_form');
+const playList = document.getElementById('play_list');
+
+// Función para agregar una canción a la lista
+
+function addSong(songName, artistName, songUrl) {
+    const listSong = document.createElement('li');
+    listSong.innerHTML = `
+            <strong> ${songName} </strong> 
+            - ${artistName}
+            <a href="${songUrl}" target="_blank" >Reproducir</a>
+            <button class="delete-btn" >Eliminar</button>
+    `;
+    //Agregar al html
+    playList.appendChild(listSong);
+
+
+
+}
+
+//Manejo de envio de formularios (Eventos)
+
+songForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    //Obtener datos del formulario
+    const songName = document.getElementById("song_name").value;
+    const artistName = document.getElementById("artist_name").value;
+    const songUrl = document.getElementById("url_song").value;
+
+    addSong(songName, artistName, songUrl);
+
+    songForm.reset();
+})
